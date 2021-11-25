7fabc5252f697e29326206276d5c5ce8e4796b20
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-11-25 19:19:26 +0700

    Update pmbot.py

--- userbot/modules/pmbot.py
+++ userbot/modules/pmbot.py
@@ -47,7 +47,7 @@ OWNER = user.first_name
 FINISHED_PROGRESS_STR = "●"
 UNFINISHED_PROGRESS_STR = "○"
 
-DEFAULT_PMBOT = f"**👋 Hai** {mention}**!**\
+DEFAULT_PMBOT = "**👋 Hai** {mention}**!**\
      \n\n**Saya adalah {my_first}** \
      \n**Anda dapat Menghubungi [{OWNER}](tg://user?id={OWNER_ID}) dari sini.**\
      \n**Jangan Melakukan Spam Atau anda akan diBanned**\
