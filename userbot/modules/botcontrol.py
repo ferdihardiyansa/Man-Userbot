fe3ac0e6d104e0bc925f90c1c66b28f84e3c84ed
mrismanaziz <mrismanaziz@gmail.com>
2021-11-26 08:22:28 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/botcontrol.py
+++ userbot/modules/botcontrol.py
@@ -11,7 +11,16 @@ from datetime import datetime
 from telethon import Button, custom
 from telethon.utils import get_display_name
 
-from userbot import BOT_USERNAME, BOTLOG, BOTLOG_CHATID, CHANNEL, GROUP, StartTime, tgbot, user
+from userbot import (
+    BOT_USERNAME,
+    BOTLOG,
+    BOTLOG_CHATID,
+    CHANNEL,
+    GROUP,
+    StartTime,
+    tgbot,
+    user,
+)
 from userbot.modules.sql_helper.bot_blacklists import check_is_black_list
 from userbot.modules.sql_helper.bot_starters import (
     add_starter_to_db,
