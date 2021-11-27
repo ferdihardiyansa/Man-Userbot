fdc6cd6e6d5b5c273b5ade29ef16110f4e4ed8bb
ferdihardiyansa <85765224+ferdihardiyansa@users.noreply.github.com>
2021-11-27 00:03:05 -0800

    Update animation.py

--- userbot/modules/animation.py
+++ userbot/modules/animation.py
@@ -520,7 +520,7 @@ async def _(typew):
 async def _(typew):
     await typew.edit("**Mau Jadi Pacarku Gak?**")
     sleep(2) 
-    await typew.edit("**kalau sak mau saya maksa**")
+    await typew.edit("**kalau gak saya maksa**")
 
     
 @bot.on(man_cmd(outgoing=True, pattern=r"bundir(?: |$)(,*)"))
