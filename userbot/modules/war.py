6c6f45d59bedcbc72f6b78c7640c0ad8ce5e3184
ferdihardiyansa <85765224+ferdihardiyansa@users.noreply.github.com>
2021-12-04 10:38:15 -0800

    Update war.py

--- userbot/modules/war.py
+++ userbot/modules/war.py
@@ -4,3 +4,14 @@ from userbot import ALIVE_NAME
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP, bot
 from userbot.events import man_cmd
+
+
+@bot.on(man_cmd(outgoing=True, pattern=r"gembel(?: |$)(.*)"))
+async def _(event):
+    await event.client.send_message(
+        event.chat_id,
+        "**BUAH PAPAYA BUAH PISANG ITULAH NAMA NAMA BUAHAN YANG LEMBEKK SAMA KAYA LU LEMBEL UDAH KAYA TAHU DI BEJEK BEJEK TERUS DI LUDAHIN GAYA GAYAAN SOK KERAS PADAHAL LU AMPAS BEGO GADA KEREN KERENNYA LUU BEGITU YANG ADA NTAR LU KENA MENTAL TERUS LU NANGIS KEJER UDAH KEJER UDAH GITU NGADU KE EMA LU MENDING LU SEKARANG CUCI KAKI CUCI MUKA TERUS TIDUR BEDO **",
+    )
+    await event.delete()
+    
+    
