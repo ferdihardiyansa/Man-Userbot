f7c54929054446dbc7d6f140594edf92f855de2e
ferdihardiyansa <85765224+ferdihardiyansa@users.noreply.github.com>
2021-12-04 21:42:49 -0800

    Update war.py

--- userbot/modules/war.py
+++ userbot/modules/war.py
@@ -23,5 +23,46 @@ async def _(event):
     )
     await event.delete()
     
-    
+    CMD_HELP.update(
+    {
+        "war": f"**Plugin : **`war`\
+        \n\n  •  **Syntax :** `{cmd}buah`\
+        \n  •  **Function : **kosa kata 1\
+        \n\n  •  **Syntax :** `{cmd}bocah`\
+        \n  •  **Function : **kosa kata 2\
+        \n\n  •  **Syntax :** `{cmd}malam`\
+        \n  •  **Function : **selamat malam\
+        \n\n  •  **Syntax :** `{cmd}aku`\
+        \n  •  **Function : **kata kata badut\
+        \n\n  •  **Syntax :** `{cmd}ayam`\
+        \n  •  **Function : **pantun\
+        \n\n  •  **Syntax :** `{cmd}gmbln`\
+        \n  •  **Function : **kata kata menggombal\
+        \n\n  •  **Syntax :** `{cmd}sdby`\
+        \n  •  **Function : **gombalan versi 2\
+        \n\n  •  **Syntax :** `{cmd}jauh`\
+        \n  •  **Function : **sad\
+        \n\n  •  **Syntax :** `{cmd}menua`\
+        \n  •  **Function : **gmbln versi 3\
+        \n\n  •  **Syntax :** `{cmd}sore`\
+        \n  •  **Function : **selamat sore\
+        \n\n  •  **Syntax :** `{cmd}jangan`\
+        \n  •  **Function : **perkataan katak bizer\
+        \n\n  •  **Syntax :** `{cmd}sokab`\
+        \n  •  **Function : **Ngeledek orang so kenal so dekat padahal kga kenal goblok\
+        \n\n  •  **Syntax :** `{cmd}gembel`\
+        \n  •  **Function : **Ngeledek bapaknya si jamet\
+        \n\n  •  **Syntax :** `{cmd}cuih`\
+        \n  •  **Function : **Ngeludahin keluarganya satu satu wkwk\
+        \n\n  •  **Syntax :** `{cmd}dih`\
+        \n  •  **Function : **Ngeledek anak haram\
+        \n\n  •  **Syntax :** `{cmd}gcs`\
+        \n  •  **Function : **Ngeledek gc sampah\
+        \n\n  •  **Syntax :** `{cmd}virtual`\
+        \n  •  **Function : **Ngeledek orang pacaran virtual\
+        \n\n**Klo mau Req, kosa kata dari lu Bisa pake Module costum. Ketik** `{cmd}help costum`\
+    "
+    }
+)
+
     
