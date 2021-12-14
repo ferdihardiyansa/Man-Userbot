6a26b6262bc0c2eb549e9c27195d7886eb84d16b
ferdihardiyansa <85765224+ferdihardiyansa@users.noreply.github.com>
2021-12-13 20:38:10 -0800

    Update helper.py

--- userbot/modules/helper.py
+++ userbot/modules/helper.py
@@ -1,30 +1,25 @@
 """ Userbot module for other small commands. """
-from userbot import ALIVE_NAME
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, bot
-from userbot.events import man_cmd
-
-# ================= CONSTANT =================
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-# ============================================
+from userbot import CMD_HELP, owner
+from userbot.utils import edit_or_reply, man_cmd
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"ihelp$"))
-async def usit(e):
-    await e.edit(
-        f"**Hai {DEFAULTUSER} Kalo Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
+@man_cmd(pattern="ihelp$")
+async def usit(event):
+    await edit_or_reply(
+        event,
+        f"**Hai {owner} Kalo Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
         f"✣ **Group Support :** [Sharing Userbot](t.me/sharinguserbot)\n"
         f"✣ **Channel Man :** [Lunatic0de](t.me/Lunatic0de)\n"
-        f"✣ **Owner Repo :** [Risman](t.me/mrismanaziz)\n"
-        f"✣ **Repo :** [Man-Userbot](https://github.com/mrismanaziz/Man-Userbot)\n"
+        f"✣ **Owner Repo :** [fer-ubot](t.me/ferdikoloay)\n"
     )
 
 
-@bot.on(man_cmd(outgoing=True, pattern=r"listvar$"))
-async def var(m):
-    await m.edit(
-        f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://telegra.ph/List-Variabel-Heroku-untuk-Man-Userbot-09-22)"
+@man_cmd(pattern="listvar$")
+async def var(event):
+    await edit_or_reply(
+        event,
+        "**Daftar Lengkap Vars Dari Man-Userbot:** [KLIK DISINI](https://telegra.ph/List-Variabel-Heroku-untuk-Man-Userbot-09-22)",
     )
 
 
@@ -35,8 +30,6 @@ CMD_HELP.update(
         \n  •  **Function : **Bantuan Untuk Man-Userbot.\
         \n\n  •  **Syntax :** `{cmd}listvar`\
         \n  •  **Function : **Melihat Daftar Vars.\
-        \n\n  •  **Syntax :** `{cmd}repo`\
-        \n  •  **Function : **Melihat Repository Man-Userbot.\
         \n\n  •  **Syntax :** `{cmd}string`\
         \n  •  **Function : **Link untuk mengambil String Man-Userbot.\
     "
